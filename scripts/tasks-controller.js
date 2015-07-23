75b9e2178ca9fc4f0d242aa24c3e92dac95ac641
shanemitchell <Shane.Mitchell@gmail.com>
2015-07-23 20:09:00 +0100

    Moved tasks controller to external scripts file

--- scripts/tasks-controller.js
+++ scripts/tasks-controller.js
@@ -1,1 +1,44 @@
-.
+tasksController = function() {		// singleton, to manage state reliably
+	
+	var taskPage;					// local parameter for the page parameter
+	var initalised = false;
+	
+	return {
+		init : function(page) {
+			
+			if (!initalised) {		// ensures Controller is only initalised once 
+				taskPage = page;
+				
+				$(taskPage).find
+				('[required="required"]').prev('label').append
+				('<span>*</span>').children('span').addClass('required');
+				
+				$(taskPage).find('tbody tr:even').addClass('even');
+				
+				$(taskPage).find('#btnAddTask').click(function(evt) {
+					alert('#btnAddTask clicked...');
+					evt.preventDefault();
+					$(taskPage).find('taskCreation').removeClass('not');
+				});
+				
+				
+				$(taskPage).find('tbody tr').click(function(evt) {
+					$(evt.target).closest('td').siblings().andSelf().toggleClass('rowHighlight');
+				});
+				
+				$(taskPage).find('#tblTasks tbody').on('click', '.deleteRow', function(evt) {
+					evt.preventDefault();
+					$(evt.target).parents('tr').remove();
+				});
+				
+				$(taskPage).find('#saveTask').click(function(evt) {
+					evt.preventDefault();
+					var task = $('form').toObject();
+					$('#taskRow').tmpl(task).appendTo($(taskPage).find('#tblTasks tbody'));
+				});
+				
+				initalised = true;
+			}
+		}
+	}
+}();
