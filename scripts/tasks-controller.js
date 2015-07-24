04f63f7333bf0697023722dd74408a8d979514ba
shanemitchell <Shane.Mitchell@gmail.com>
2015-07-24 17:23:28 +0100

    Corrected typos in jQuery

--- scripts/tasks-controller.js
+++ scripts/tasks-controller.js
@@ -10,17 +10,15 @@ tasksController = function() {		// singleton, to manage state reliably
 				taskPage = page;
 				
 				$(taskPage).find
-				('[required="required"]').prev('label').append
-				('<span>*</span>').children('span').addClass('required');
+					('[required="required"]').prev('label').append
+					('<span>*</span>').children('span').addClass('required');
 				
 				$(taskPage).find('tbody tr:even').addClass('even');
 				
 				$(taskPage).find('#btnAddTask').click(function(evt) {
-					alert('#btnAddTask clicked...');
-					evt.preventDefault();
-					$(taskPage).find('taskCreation').removeClass('not');
+					evt.preventDefault();					
+					$(taskPage).find('#taskCreation').removeClass('not');
 				});
-				
 				
 				$(taskPage).find('tbody tr').click(function(evt) {
 					$(evt.target).closest('td').siblings().andSelf().toggleClass('rowHighlight');
